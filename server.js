af85dc3254220ef4220531beb706e2d2a28e2d47
chaeeun037 <chaeeun.dev@gmail.com>
2021-07-12 20:43:10 +0900

    Feature : cors 설정

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const http = require("http");
-const express = require("express");
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 
 app.set('port', process.env.PORT || 8000);
 app.use(express.json());
+
+app.use(cors());
+
+app.get('/test', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send('hi')
+})
 
 server.listen(app.get('port'), (req, res) => {
     console.log('Express server listening on port ' + app.get('port'));
