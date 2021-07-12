adf9ef658a0af2444d17ff15a7ba1e44870a83af
cothis <minho1030@naver.com>
2021-07-12 21:17:19 +0900

    Feature : db.js 추가, server.js 샘플 코드 작성중

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const { db } = require('./db');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,6 +10,12 @@ app.set('port', process.env.PORT || 8000);
 app.use(express.json());
 
 app.use(cors());
+
+//simple query
+db.query(`SELECT * FROM TEST`).then((rows, fields) => {
+  console.log(rows);
+  console.log(fields);
+});
 
 app.get('/test', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
