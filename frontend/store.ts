ec80a311faff642814b653adc630cee919b67d8e
cothis <minho1030@naver.com>
2021-07-14 15:37:51 +0900

    Feature : store.ts Store class 작성

--- frontend/store.ts
+++ frontend/store.ts
@@ -1,4 +1,50 @@
-import { User, Category, Product, ChatRoom, Town, UserTown, Picture, Wish, Message } from '../types';
+import {
+  UserStore,
+  ProductStore,
+  User,
+  Category,
+  Product,
+  ChatRoom,
+  Town,
+  UserTown,
+  Picture,
+  Wish,
+  Message,
+} from '../types';
 
-export default class Store {
-} 
+export default class Store implements UserStore, ProductStore {
+  private _user: User | null;
+  private _auth: string;
+  private _currentPage: number;
+  private products: Product[];
+  constructor() {
+    this._user = null;
+    this._auth = '';
+    this._currentPage = 1;
+    this.products = [];
+  }
+
+  get currentPage(): number {
+    return this._currentPage;
+  }
+
+  set currentPage(number: number) {
+    this._currentPage = number;
+  }
+
+  get user(): User | null {
+    return this._user;
+  }
+
+  get auth(): string {
+    return this._auth;
+  }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getProduct(id: number): Product | undefined {
+    return this.products.find((product) => product.id === id);
+  }
+}
--- types/index.ts
+++ types/index.ts
@@ -1,86 +1,86 @@
-import View from '../frontend/core/view'
+import View from '../frontend/core/view';
 
 export interface UserStore {
-    user: User;
-    auth: string;
+  user: User | null;
+  auth: string;
 }
 
 export interface ProductStore {
-    getAllProducts: () => Product[];
-    getProduct: () => Product;
-    currentPage: number;
+  getAllProducts: () => Product[];
+  getProduct: (id: number) => Product | undefined;
+  currentPage: number;
 }
 
 export interface User {
-    readonly id: number;
-    readonly email: string;
+  readonly id: number;
+  readonly email: string;
 }
 
 export interface Category {
-    readonly id: number;
-    readonly name: string;
-    readonly imagePath: string;
+  readonly id: number;
+  readonly name: string;
+  readonly imagePath: string;
 }
 
 export interface Product {
-    readonly id: number;
-    subject: string;
-    categoryId: number;
-    price?: number;
-    content?: string;
-    readonly sellerId: number;
-    state: number;
-    views: number;
-    readonly createdAt: Date;
+  readonly id: number;
+  subject: string;
+  categoryId: number;
+  price?: number;
+  content?: string;
+  readonly sellerId: number;
+  state: number;
+  views: number;
+  readonly createdAt: Date;
 
-    // db 외 정보
-    pictures: Picture[];
-    userWish: boolean;
-    wishes: number;
-    chatRooms: number;
+  // db 외 정보
+  pictures: Picture[];
+  userWish: boolean;
+  wishes: number;
+  chatRooms: number;
 }
 
 export interface ChatRoom {
-    readonly id: number;
-    readonly productId: number;
-    readonly sellerId: number;
-    readonly customerId: number
+  readonly id: number;
+  readonly productId: number;
+  readonly sellerId: number;
+  readonly customerId: number;
 }
 
 export interface Town {
-    readonly id: number;
-    readonly name: string;
+  readonly id: number;
+  readonly name: string;
 }
 
 export interface UserTown {
-    readonly id: number;
-    readonly userId: number;
-    readonly townId: number;
+  readonly id: number;
+  readonly userId: number;
+  readonly townId: number;
 }
 
 export interface Picture {
-    readonly id: number;
-    readonly path: string;
-    readonly productId: number;
+  readonly id: number;
+  readonly path: string;
+  readonly productId: number;
 }
 
 export interface Wish {
-    readonly id: number;
-    readonly userId: number;
-    readonly productId: number;
-    isChecked: boolean;
+  readonly id: number;
+  readonly userId: number;
+  readonly productId: number;
+  isChecked: boolean;
 }
 
 export interface Message {
-    readonly id: number;
-    readonly chatRoomId: number;
-    readonly userId: number;
-    readonly content: string;
-    isRead: boolean;
-    readonly createdAt: Date;
+  readonly id: number;
+  readonly chatRoomId: number;
+  readonly userId: number;
+  readonly content: string;
+  isRead: boolean;
+  readonly createdAt: Date;
 }
 
 export interface RouteInfo {
-    path: string;
-    page: View;
+  path: string;
+  page: View;
 }
