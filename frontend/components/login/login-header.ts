5b72691abde0da8212b1cbcb767caaa8f3813477
cothis <minho1030@naver.com>
2021-07-15 21:06:26 +0900

    Fix : login header 가운데 정렬

--- frontend/components/login/login-header.ts
+++ frontend/components/login/login-header.ts
@@ -2,11 +2,11 @@ import View from '../../core/view';
 import Store from '../../store';
 
 const template = `
-<button type="button">
+<button type="button" class="icon-wrapper">
   <img src="./frontend/static/images/icons/chevron-left.png">
 </button>  
 <span class="text medium">로그인</span>
-<button type="button"></button>
+<button type="button" class="icon-wrapper"></button>
 `;
 
 export default class LoginHeaderComponent extends View {
