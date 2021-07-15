6cee21b50c0b6bd55298c9c6c1bbb3d3d548e526
cothis <minho1030@naver.com>
2021-07-15 23:11:25 +0900

    Feature : 배포 전 경로 문제 해결

--- frontend/components/common/header.ts
+++ frontend/components/common/header.ts
@@ -5,22 +5,22 @@ import Store from '../../core/store';
 const template = `
 <div class="header-container flex grow">
     <div class="header-center link medium white flex ai-center jc-center">
-      <img src="/frontend/static/images/icons/map-pin.png">
+      <img src="/static/images/icons/map-pin.png">
       <div>장소</div>
     </div>
     <div class="header-left">
-      <img src="/frontend/static/images/icons/category.png">
+      <img src="/static/images/icons/category.png">
     </div>
     <div class="header-right flex">
-      <img src="/frontend/static/images/icons/user.png">
-      <img id="headerMenu" src="/frontend/static/images/icons/menu.png">
+      <img src="/static/images/icons/user.png">
+      <img id="headerMenu" src="/static/images/icons/menu.png">
     </div>
 </div>
 `;
 
 export default class Header extends View {
   private store: Store;
-//   private api: ProductApi;
+  //   private api: ProductApi;
 
   constructor(selector: string, store: Store) {
     super(selector, template);
@@ -36,6 +36,6 @@ export default class Header extends View {
     this.updateView();
 
     const headerMenu = document.querySelector('#headerMenu');
-    headerMenu!.addEventListener('click', this.onMenuClickHandler)
+    headerMenu!.addEventListener('click', this.onMenuClickHandler);
   }
 }
