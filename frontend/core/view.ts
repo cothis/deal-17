f0a54770f8ada751473fcb1205315103c1e4e8e9
cothis <minho1030@naver.com>
2021-07-18 13:00:15 +0900

    Feature : view 클래스 appendView 및 remove 구현

--- frontend/core/view.ts
+++ frontend/core/view.ts
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { AnimateType } from '../../types';
 
 export default abstract class View {
@@ -5,7 +6,9 @@ export default abstract class View {
   private renderTemplate: string;
   private container: HTMLElement;
   private htmlList: string[];
-  private element?: HTMLElement;
+  private pageContainer?: HTMLElement;
+  private renderAnimate?: AnimateType;
+  private removeAnimate?: AnimateType;
 
   constructor(selector: string, template: string) {
     const containerElement = <HTMLElement>document.querySelector(selector);
@@ -23,6 +26,27 @@ export default abstract class View {
   protected updateView(): void {
     this.container.innerHTML = this.renderTemplate;
     this.renderTemplate = this.template;
+  }
+
+  protected appendView(renderAnimate?: AnimateType, removeAnimate?: AnimateType): void {
+    this.renderAnimate = renderAnimate;
+    this.removeAnimate = removeAnimate;
+
+    this.pageContainer = document.createElement('section');
+    this.pageContainer.innerHTML = this.renderTemplate;
+    this.renderTemplate = this.template;
+
+    this.pageContainer.classList.add('container');
+    if (this.renderAnimate) {
+      this.pageContainer.classList.add(`animate-${this.renderAnimate.toString()}`);
+    }
+    this.container.appendChild(this.pageContainer);
+
+    if (this.renderAnimate) {
+      requestAnimationFrame(() => {
+        this.pageContainer?.classList.remove(`animate-${this.renderAnimate?.toString()}`);
+      });
+    }
   }
 
   protected appendComponent(): void {
@@ -51,5 +75,12 @@ export default abstract class View {
   }
 
   abstract render(): void;
-  remove(): void {}
+  remove(): void {
+    if (this.removeAnimate) {
+      this.pageContainer?.addEventListener('transitionend', this.pageContainer.remove);
+      this.pageContainer?.classList.add(`animate-${this.removeAnimate.toString()}`);
+    } else {
+      this.pageContainer?.remove();
+    }
+  }
 }
