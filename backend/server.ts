02fffb553be71e6bf0c0156181bcaa95594e3409
chaeeun037 <chaeeun.dev@gmail.com>
2021-07-19 17:20:53 +0900

    Feature : wish api 개발

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import 'dotenv-defaults/config';
 import ProductController from './products/product.controller';
 import PictureController from './pictures/picture.controller';
 import UserController from './users/user.controller'
+import WishController from './wishes/wish.controller'
 import * as path from 'path';
 
 const app = express();
@@ -20,6 +21,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html
 app.use('/api/v0/products', ProductController);
 app.use('/api/v0/pictures', PictureController);
 app.use('/api/v0/users', UserController);
+app.use('/api/v0/wishes', WishController);
 
 server.listen(app.get('port'), () => {
   console.log('Express server listening on port ' + app.get('port'));
