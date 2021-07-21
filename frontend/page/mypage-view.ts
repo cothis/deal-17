7b365f0c1dfb0a1121e2a48abb36dbb1764d7ed6
cothis <minho1030@naver.com>
2021-07-21 11:12:59 +0900

    Feature : login backend camelCase 적용

--- frontend/page/mypage-view.ts
+++ frontend/page/mypage-view.ts
@@ -28,7 +28,8 @@ export default class MypageView extends View {
   }
 
   render() {
-    this.setTemplateData('username', 'test user');
+    console.log(this.store.user);
+    this.setTemplateData('username', this.store.user?.email!);
     this.appendView(AnimateType.RIGHT, AnimateType.RIGHT);
     new HeaderComponent('#mypageView__header', this.store, { title: '내 계정' }).render();
     new LoginButtonComponent('#myPage', this.store, { title: '로그아웃', id: 'logout', onClick: () => {} }).render();
