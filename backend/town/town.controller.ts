ea6515d9b89aa07599944555fcd4c8446d7a35f9
cothis <minho1030@naver.com>
2021-07-22 19:19:35 +0900

    Feature : userTown 생성 api 작성

--- backend/town/town.controller.ts
+++ backend/town/town.controller.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getTownsByUserId } from '../user-town/user-town.service';
+import { addUserTown, getTownsByUserId } from '../user-town/user-town.service';
 import { getOrAddTown } from './town.service';
 
 const router = Router();
@@ -26,6 +26,25 @@ router.get('/', (req, res) => {
     getTownsByUserId(parseInt(String(userId))).then((user) => {
       res.json(user);
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post('/', (req, res) => {
+  try {
+    const { userId, townName } = req.body;
+    if (!userId) throw new Error('유저 아이디가 필요합니다.');
+    if (!townName) throw new Error('동네 이름이 필요합니다.');
+
+    getOrAddTown(String(townName))
+      .then((town) => {
+        addUserTown(userId, town.id);
+      })
+      .then((userTownId) => {
+        res.json({ result: true, userTownId });
+      });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message });
