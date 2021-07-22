3330a5bfb6815a4af3442335b1f05ff8f3bcb396
cothis <minho1030@naver.com>
2021-07-22 16:16:40 +0900

    Feature : api.ts TownApi 클래스 작성

--- frontend/core/api.ts
+++ frontend/core/api.ts
@@ -1,4 +1,4 @@
-import { Product, Picture, ChatRoom, Wish, User } from '../../types';
+import { Product, Picture, ChatRoom, Wish, User, Town } from '../../types';
 import qs from 'querystring';
 
 const BASE_URL: string = process.env.API_URL ?? 'http://localhost:8000';
@@ -122,5 +122,15 @@ export class ChatRoomApi extends Api {
 
   getChatRoomByProductId(productId: number): Promise<ChatRoom[]> {
     return this.request<ChatRoom[]>(`/${productId}`);
+  }
+}
+
+export class TownApi extends Api {
+  constructor() {
+    super('/api/v0/town');
+  }
+
+  getTownsByUserId(id: number): Promise<Town[]> {
+    return this.request<Town[]>(`?userId=${id}`);
   }
 }
